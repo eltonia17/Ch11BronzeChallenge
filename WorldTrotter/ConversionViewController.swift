4e5e0ba8c74af4c3672943a87aaa96f391709770
Leonard <esl107@miami.edu>
2025-11-01 00:58:25 -0400

    Chapter 11 Full Bronze challenge (both quiz and worldtrotter)

--- WorldTrotter/ConversionViewController.swift
+++ WorldTrotter/ConversionViewController.swift
@@ -4,25 +4,25 @@
 //
 //  Created by Eltonia Leonard on 9/20/25.
 //
-
 import UIKit
 
-class ViewController: UIViewController {
-
+class ConversionViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        override func viewWillAppear(_ animated: Bool) {
-                super.viewWillAppear(animated)
-                // Change background each time this screen is about to appear
-                view.backgroundColor = UIColor(
-                    hue: .random(in: 0...1),
-                    saturation: .random(in: 0.4...0.7),
-                    brightness: .random(in: 0.9...1.0),
-                    alpha: 1.0
-                )
+        setRandomBackground()
     }
 
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        setRandomBackground()
+    }
 
+    private func setRandomBackground() {
+        view.backgroundColor = UIColor(
+            hue: CGFloat.random(in: 0...1),
+            saturation: CGFloat.random(in: 0.4...0.7),
+            brightness: CGFloat.random(in: 0.9...1.0),
+            alpha: 1.0
+        )
+    }
 }
-
